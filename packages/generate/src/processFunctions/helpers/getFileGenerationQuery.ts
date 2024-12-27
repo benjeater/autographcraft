f2577b18ccf1284ae3281645d8298cce3dd2bfbe
Ben Jeater <39923928+benjeater@users.noreply.github.com>
2024-12-27 10:40:51 +0000

    Fix generate query and bump generate package

--- packages/generate/src/processFunctions/helpers/getFileGenerationQuery.ts
+++ packages/generate/src/processFunctions/helpers/getFileGenerationQuery.ts
@@ -2,12 +2,7 @@ export function getFileGenerationQuery() {
   return `query GenerateFiles($configuration: String!, $schema: String!) {
     generate(configuration: $configuration, schema: $schema) {
       executionDuration
-      files {
-        filePath
-        content
-        addIgnoreHeader
-        shouldOverwrite
-      }
+      signedUrl
     }
   }
   `;
