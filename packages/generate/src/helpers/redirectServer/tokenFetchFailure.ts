02da9f63f348cf7e19f2beee77a9f070a23c970f
Ben Jeater <39923928+benjeater@users.noreply.github.com>
2024-12-31 09:34:02 +0000

    Add html content for token success and failure

---
+++ packages/generate/src/helpers/redirectServer/tokenFetchFailure.ts
@@ -0,0 +1,13 @@
+export const FAILURE_HTML = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>AutoGraphCraft</title>
+</head>
+<body>
+  <h1 style="text-align: center;color: red;">Invalid request; unique_ref not found or did not match expected value</h1>
+  <p style="text-align: center;">Please close this tab and try again</p>
+</body>
+</html>
+`;
