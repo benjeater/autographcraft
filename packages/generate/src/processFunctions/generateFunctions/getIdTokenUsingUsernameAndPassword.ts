295fd91a9f5e676ad677392be0737ec5708fa9ee
Ben Jeater <benjeater@gmail.com>
2025-01-12 18:45:47 +0000

    Added TODO

--- packages/generate/src/processFunctions/generateFunctions/getIdTokenUsingUsernameAndPassword.ts
+++ packages/generate/src/processFunctions/generateFunctions/getIdTokenUsingUsernameAndPassword.ts
@@ -3,6 +3,9 @@ export function getIdTokenUsingUsernameAndPassword(
   password: string
 ): Promise<string> {
   return new Promise<string>((resolve) => {
+    // TODO: Implement call to Cognito endpoint.  This will require a DNS entry to allow
+    // the endpoint to be static.
+
     resolve('idToken');
   });
 }
